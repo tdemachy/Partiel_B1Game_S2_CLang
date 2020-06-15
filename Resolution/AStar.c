72340ddb54952b785a422a5b6e3ff86e6e847f7a
Thomas Demachy <tdemachy@free.fr>
2020-06-15 12:27:09 +0200

    Framework de correction

---
+++ Resolution/AStar.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#define WIDTH 10
+#define HEIGHT 10
+
+// ********** MAIN ***********************
+int main(void)
+{
+
+	// Votre code ici
+
+	return 0;
+}
